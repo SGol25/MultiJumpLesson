887259caf736db8a80fe8e34466b87d6070a4b20
SGol25 <dselenchuk@gmail.com>
2025-10-08 00:37:45 +1000

    OverLay

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Death : MonoBehaviour
 {
     private GameObject Player;
+    
     private void Start()
     {
         Player = GameObject.Find("Player");
+        
     }
 
     void OnTriggerEnter(Collider other)
@@ -17,6 +19,7 @@ public class Death : MonoBehaviour
             // Телепортируем его на старт
             Debug.Log("Игрок умер");
             Player.transform.position = new Vector3(-17f, 2f, 0.8f);
+
         }
 
     }
