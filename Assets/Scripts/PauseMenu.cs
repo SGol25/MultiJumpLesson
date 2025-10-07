2f8689361645b1290520ef4e984aa29f66ecee07
SGol25 <dselenchuk@gmail.com>
2025-10-08 01:44:11 +1000

    Add PauseMenu
    Добавлена меню паузы (кнопка Esc)

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,16 +1,60 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject pausePanel;
+
+    private bool isPaused = false;
+
     void Start()
     {
-        
+        // Убедимся, что панель выключена при старте
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Проверяем нажатие клавиши ESC
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if (pausePanel != null)
+            pausePanel.SetActive(true);
+
+        Time.timeScale = 0f; // Останавливаем время
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        if (pausePanel != null)
+            pausePanel.SetActive(false);
+
+        Time.timeScale = 1f; // Возвращаем время
+        isPaused = false;
+    }
+
+    // Выход в меню
+    public void QuitToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Рестарт игры
+    public void Restart()
+    {
+        Resume();
+        SceneManager.LoadScene("SampleScene");
     }
 }
